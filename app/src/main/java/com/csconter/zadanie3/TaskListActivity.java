004b083bd01f59e85c129849ea3dbf1b2e15a6a7
JanuaryDev <janofficial2002@gmail.com>
2023-11-13 11:16:50 +0100

    Initial commit

---
+++ app/src/main/java/com/csconter/zadanie3/TaskListActivity.java
@@ -0,0 +1,9 @@
+package com.csconter.zadanie3;
+
+import androidx.fragment.app.Fragment;
+public class TaskListActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new TaskListFragment();
+    }
+}
